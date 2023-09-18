de7ffb6af00bf086595bbccafc2b74fd53de91ba
daceyforward <dacey.loving@gmail.com>
2023-09-18 13:19:11 -0400

    adds django authentication, users can sign up, login, and logout

--- birthdayblaster/main_app/views.py
+++ birthdayblaster/main_app/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView 
 from django.views.generic import ListView, DetailView
 
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from.models import Birthday, GiftIdeas
 
+#  -----------------------------------------
 
 def home(request):
     return render(request, 'home.html')
@@ -15,45 +19,72 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def birthdays_index(request):
-  birthdays= Birthday.objects.all()
+  birthdays= Birthday.objects.filter(user=request.user)
   # We pass data to a template very much like we did in Express!
   return render(request, 'birthdays/index.html', {
     'birthdays': birthdays
   })
 
+@login_required
 def birthdays_detail(request, birthday_id):
   birthday = Birthday.objects.get(id=birthday_id)
   return render(request, 'birthdays/detail.html', { 'birthday': birthday })
 
-class BirthdayCreate(CreateView):
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+        # This will add the user to the database
+            user = form.save()
+        # This is how we log a user in via code
+            login(request, user)
+            return redirect('index')
+    else:
+        error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+#  CBVs -----------------------------------------
+
+class BirthdayCreate(LoginRequiredMixin, CreateView):
   model = Birthday
   fields = '__all__'
 
-class BirthdayUpdate(UpdateView):
+  def form_valid(self, form):
+    form.instance.user = self.request.user 
+    return super().form_valid(form)
+
+class BirthdayUpdate(LoginRequiredMixin, UpdateView):
   model = Birthday
   # Let's disallow the renaming of a cat by excluding the name field!
   fields = '__all__'
 
-class BirthdayDelete(DeleteView):
+class BirthdayDelete(LoginRequiredMixin, DeleteView):
   model = Birthday
   success_url = '/birthdays'
 
-class GiftList(ListView):
+class GiftList(LoginRequiredMixin, ListView):
    model=GiftIdeas  
 
-class GiftDetail(DeleteView):
+class GiftDetail(LoginRequiredMixin, DeleteView):
    model=GiftIdeas
 
-class GiftCreate(CreateView):
+class GiftCreate(LoginRequiredMixin, CreateView):
    model= GiftIdeas
    fields = '__all__'
 
-class GiftUpdate(UpdateView):
+class GiftUpdate(LoginRequiredMixin, UpdateView):
    model=GiftIdeas
    fields= '__all__'
 
-class GiftDelete(DeleteView):
+class GiftDelete(LoginRequiredMixin, DeleteView):
    model = GiftIdeas
    success_url = '/gifts'
                
